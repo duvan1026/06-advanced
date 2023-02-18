a1a423261e13d0abb12de4e403acaeacc76fce87
Duvan Castro <duvancastro1026@gmail.com>
2023-02-18 18:50:56 -0500

    first example of callbacks in JavaScript

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -1,3 +1,4 @@
+import { heroes } from "../data/heroes"
 
 /**
  * 
@@ -5,5 +6,23 @@
  */
 export const callbaksComponent = ( element ) => {
 
-        console.log('callbaksComponent');
+        const id = '5d86371fd55e2e2a30fe1ccb1';
+        findHero( id, (hero) => { // Se coloca cualquien nombre en la funcion
+
+                element.innerHTML = hero.name;
+        });
+
+}
+
+/**
+ * 
+ * @param {String} id 
+ * @param { (hero: Object)=> void } callback 
+ */
+const findHero = ( id, callback ) => {
+
+        const hero = heroes.find( hero => hero.id === id );  
+
+        callback( hero );
+
 }
