91ad6d19c50abf527fd8b3ee60afa9738d7f4ca0
Duvan Castro <duvancastro1026@gmail.com>
2023-03-02 05:13:43 -0500

    example implemented about using await

--- src/concepts/06-async-await.js
+++ src/concepts/06-async-await.js
@@ -1,9 +1,28 @@
+import { heroes } from "../data/heroes"
 
 /**
  * 
  * @param {HTMLDivElement} element 
  */
-export const asyncAwaitComponent = ( element ) => {
+export const asyncAwaitComponent = async( element ) => {
 
-        console.log('asyncAwaitComponent');
+        const id1 = '5d86371f2343e37870b91ef1';
+        const id2 = '5d86371f25a058e5b1c8a65e';
+
+
+        const hero1 = await findHero( id1 );// Todo (await)  me devuelve el objeto que estoy esperando, no una promesa 
+        const hero2 = await findHero( id2 );// Todo (await)  me devuelve el objeto que estoy esperando, no una promesa 
+        
+        element.innerHTML = `${ hero1.name } / ${ hero2.name }`;
+
+
+}
+
+const findHero = async( id ) => {
+
+        const hero = heroes.find( hero => hero.id === id );
+        if( !hero )
+                throw `Hero not found`;
+
+        return hero;
 }
