f53714b50454d57f587add2e5f19b0aeba28b057
Duvan Castro <duvancastro1026@gmail.com>
2023-03-02 04:48:24 -0500

    function finHero created and Implemented with Promise async

--- src/concepts/05-async.js
+++ src/concepts/05-async.js
@@ -1,3 +1,4 @@
+import { heroes } from "../data/heroes"
 
 /**
  * 
@@ -5,5 +6,21 @@
  */
 export const asyncComponent = ( element ) => {
 
-        console.log('asyncComponent');
+        const id1 = '5d86371fd55e2e2a30fe1ccb';
+
+        findHero( id1 )
+                .then( name => element.innerHTML = name )
+                .catch( error => element.innerHTML = error )
+
+}
+/**
+ * Funcion que me regresara el heroe correspondiente al ID ingresado
+ * @param {String} id 
+ * @returns  {Promise<String>}
+ */
+const findHero = async( id ) => {//Todo (async) transforma la función a regresar una promesa
+
+        const hero = heroes.find( hero => hero.id === id );
+
+        return hero?.name;
 }
