c3ed63d218af1a83883a08358c08383e4630b386
Duvan Castro <duvancastro1026@gmail.com>
2023-03-02 05:35:42 -0500

    Error handling in async-await

--- src/concepts/06-async-await.js
+++ src/concepts/06-async-await.js
@@ -9,13 +9,15 @@ export const asyncAwaitComponent = async( element ) => {
         const id1 = '5d86371f2343e37870b91ef1';
         const id2 = '5d86371f25a058e5b1c8a65e';
 
-
-        //Se puede aplicar desestructuraciones de los valores recibidos
-        const { name: name1 } = await findHero( id1 );// Todo (await)  me devuelve el objeto que estoy esperando, no una promesa 
-        const { name: name2 } = await findHero( id2 );// Todo (await)  me devuelve el objeto que estoy esperando, no una promesa 
-        
-        element.innerHTML = `${ name1 } / ${ name2 }`;
-
+        try {
+                const hero1 = await findHero( id1 );// Todo (await)  me devuelve el objeto que estoy esperando, no una promesa 
+                const hero2 = await findHero( id2 );// Todo (await)  me devuelve el objeto que estoy esperando, no una promesa 
+                
+                element.innerHTML = `${ hero1.name } / ${ hero2.name }`;        
+                
+        } catch (error) {
+               element.innerHTML = error; 
+        }
 
 }
 
@@ -23,7 +25,7 @@ const findHero = async( id ) => {
 
         const hero = heroes.find( hero => hero.id === id );
         if( !hero )
-                throw `Hero not found`;
+                throw `Hero ${ id } not found`;
 
         return hero;
 }
