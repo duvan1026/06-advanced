f4cb9bb903cfe8edaf6d6287db0017b8d115d8e8
Duvan Castro <duvancastro1026@gmail.com>
2023-03-02 04:13:16 -0500

    implement of Promise Race en Javascript

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import javascriptLogo from './javascript.svg'
 import { environmentsComponent } from './src/concepts/01-environments';
 import { callbaksComponent } from './src/concepts/02-callbacks';
 import { promiseComponent } from './src/concepts/03-promises';
+import { promiseRaceComponent } from './src/concepts/04-promise-race';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -23,5 +24,7 @@ const element = document.querySelector('.card');
 
 // environmentsComponent( element );
 // callbaksComponent( element );
-promiseComponent( element );
+// promiseComponent( element );
+promiseRaceComponent( element );
+
 
