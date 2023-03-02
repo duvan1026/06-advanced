fba9765337f5144c3fcd1e95f5c7f7f15178f17a
Duvan Castro <duvancastro1026@gmail.com>
2023-03-02 05:47:01 -0500

    validation process the ejecution the promeses in waterfall

--- src/concepts/07-async-await.js
+++ src/concepts/07-async-await.js
@@ -3,7 +3,56 @@
  * 
  * @param {HTMLDivElement} element 
  */
-export const asyncAwait2Component = ( element ) => {
+export const asyncAwait2Component = async( element ) => {
 
-        console.log('asyncAwait2Component');
+        console.time('Start');
+
+        const value1 = await slowPromise();
+        const value2 = await mediumPromise();
+        const value3 = await fastPromise();
+
+        element.innerHTML =`
+                value1: ${ value1 } <br>
+                value2: ${ value2 } <br>
+                value3: ${ value3 } <br>
+
+                `
+        console.timeEnd('Start');
 }
+
+/**
+ * Funcion que regresa una promesa, forma corta
+ * @returns 
+ */
+const slowPromise = () => new Promise( resolve => {
+
+        setTimeout(() => {// Todo Funcion de javascript sirve para ejecutar en un tiempo especifico la función
+                 resolve('Slow Promise');
+         },2000);
+ 
+ });
+ 
+ 
+ /**
+  * Funcion que regresa una promesa, forma corta
+  * @returns 
+  */
+ const mediumPromise = () => new Promise( resolve => {
+ 
+         setTimeout(() => {// Todo Funcion de javascript sirve para ejecutar en un tiempo especifico la función
+                  resolve('Medium Promise');
+          },1500);
+  
+  });
+
+  /**
+  * Funcion que regresa una promesa, forma corta
+  * @returns 
+  */
+ const fastPromise = () => new Promise( resolve => {
+ 
+         setTimeout(() => {// Todo Funcion de javascript sirve para ejecutar en un tiempo especifico la función
+                  resolve('Fast Promise');
+          },1000);
+  
+  });
