fca1d3f88068581afb9639ac89092f3e6688129a
Duvan Castro <duvancastro1026@gmail.com>
2023-03-02 05:02:19 -0500

    function asyncAwaitComponente() created and implemented

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { callbaksComponent } from './src/concepts/02-callbacks';
 import { promiseComponent } from './src/concepts/03-promises';
 import { promiseRaceComponent } from './src/concepts/04-promise-race';
 import { asyncComponent } from './src/concepts/05-async';
+import { asyncAwaitComponent } from './src/concepts/06-async-await';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -27,6 +28,7 @@ const element = document.querySelector('.card');
 // callbaksComponent( element );
 // promiseComponent( element );
 // promiseRaceComponent( element );
-asyncComponent( element );
+// asyncComponent( element );
+asyncAwaitComponent( element );
 
 
