d1713aa64b6c232dbd88ca0e6286e064ad3f37b6
Duvan Castro <duvancastro1026@gmail.com>
2023-03-02 05:37:33 -0500

    Sequential Promese handling

--- src/concepts/06-async-await.js
+++ src/concepts/06-async-await.js
@@ -9,6 +9,8 @@ export const asyncAwaitComponent = async( element ) => {
         const id1 = '5d86371f2343e37870b91ef1';
         const id2 = '5d86371f25a058e5b1c8a65e';
 
+        //Todo Manejo de promesas secuenciales con su respectivo manejo de errores
+
         try {
                 const hero1 = await findHero( id1 );// Todo (await)  me devuelve el objeto que estoy esperando, no una promesa 
                 const hero2 = await findHero( id2 );// Todo (await)  me devuelve el objeto que estoy esperando, no una promesa 
