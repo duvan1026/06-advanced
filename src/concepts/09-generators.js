bab16e339ca825e882ec2deb5956276f3d25b349
Duvan Castro <duvancastro1026@gmail.com>
2023-03-03 05:07:05 -0500

    implementation and  exacution example the generator function with counter

--- src/concepts/09-generators.js
+++ src/concepts/09-generators.js
@@ -5,28 +5,50 @@
  */
 export const generatorFunctionsComponent = ( element ) => {
 
-        const myGenerator = myFirstGeneratorFunction();
+        // const myGenerator = myFirstGeneratorFunction();
+        // console.log( myGenerator.next() );
+
+        const genId = idGenerator();
+
+        const button = document.createElement('button'); // Todo: crea un boton
+         button.innerText = 'Click me';
+
+         element.append( button );
 
 
-        console.log( myGenerator.next() );
-        console.log( myGenerator.next() );
-        console.log( myGenerator.next() );
-        console.log( myGenerator.next() );
-        console.log( myGenerator.next() );
+         const renderButton = () => {
+                const { value } = genId.next();
+                button.innerText = `Click ${ value }`;//Todo: concatena el valor del value en el botton
+        }
 
 
+        button.addEventListener('click', renderButton );
+        
         
 }
 
 
+function* idGenerator(){
 
-function* myFirstGeneratorFunction() {
+        let currentId = 0;
 
-        yield 'Primer valor';//Todo: funciona similar al return, pero cuando se necuentra se pausa el programa
-        yield 'Segundo valor';
-        yield 'tercer valor';
-        yield 'Cuarto valor';
+        while(true) {
+                yield ++currentId; // incrementa y luego me regresa el valor
+        }
 
-        return 'Ya no hay valores';
-        yield 'Ya no se puede hacer nada';
 }
+
+
+
+
+//Todo: referencia de funcion generadora
+// function* myFirstGeneratorFunction() {
+
+//         yield 'Primer valor';//Todo: funciona similar al return, pero cuando se necuentra se pausa el programa
+//         yield 'Segundo valor';
+//         yield 'tercer valor';
+//         yield 'Cuarto valor';
+
+//         return 'Ya no hay valores';
+//         yield 'Ya no se puede hacer nada';
+// }
