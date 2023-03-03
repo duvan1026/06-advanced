3d7b39f8c5b5d54705a1d56940d60d8a79d789c5
Duvan Castro <duvancastro1026@gmail.com>
2023-03-03 04:46:58 -0500

    created myFirstGeneratorFunction function generator and implemented

--- src/concepts/09-generators.js
+++ src/concepts/09-generators.js
@@ -5,5 +5,30 @@
  */
 export const generatorFunctionsComponent = ( element ) => {
 
-        console.log('generatorFunctionsComponent');
+        const myGenerator = myFirstGeneratorFunction();
+
+
+        console.log( myGenerator.next() );
+        console.log( myGenerator.next() );
+        console.log( myGenerator.next() );
+
+        console.log( myGenerator.next() );
+
+        console.log( myGenerator.next() );
+
+
+        
+}
+
+
+
+function* myFirstGeneratorFunction() {
+
+        yield 'Primer valor';//Todo: funciona similar al return, pero cuando se necuentra se pausa el programa
+        yield 'Segundo valor';
+        yield 'tercer valor';
+        yield 'Cuarto valor';
+
+        return 'Ya no hay valores';
+        yield 'Ya no se puede hacer nada';
 }
