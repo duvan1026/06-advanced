befc97217200088a315b1ef642c79851d0db5d5b
Duvan Castro <duvancastro1026@gmail.com>
2023-03-03 05:36:35 -0500

    implementation example for generator async function

--- src/concepts/10-generators-async.js
+++ src/concepts/10-generators-async.js
@@ -1,9 +1,48 @@
+import { heroes } from "../data/heroes";
 
 /**
  * 
  * @param {HTMLDivElement} element 
  */
-export const generatorsAsyncComponent = ( element ) => {
+export const generatorsAsyncComponent = async( element ) => {
 
-        console.log('generatorsAsyncComponent');
+        const heroGenerator = getHeroGenerator();
+        let isFinished = false;
+
+
+        do {
+                const { value, done } = await heroGenerator.next();
+                isFinished = done;
+                if( isFinished ) break; // Todo: si el done es true salimos del ciclo
+
+                element.innerHTML = value;
+                
+        } while ( !isFinished );
+
+}
+
+
+async function* getHeroGenerator() {
+        
+        for( const hero of heroes ){
+                await sleep();//Todo: espera hasta que finalice la promise dentro de la funcion sleep
+                yield hero.name;
+                
+        }
+        return 'no hay mas valores';
+}
+
+
+/**
+ * Espera un segundo para su resolución
+ * @returns {Promise}
+ */
+const sleep = () => {
+
+        return new Promise (( resolve ) => {
+                setTimeout(() => {
+                        resolve();
+                }, 500);        
+        });
+
 }
